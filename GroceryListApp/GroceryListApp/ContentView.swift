79fa16d86277265ec0b323b1014cf1495c29a28b
Dhruv Patel <dhruvpatel913@gmail.com>
2026-05-23 21:01:29 -0700

    AppIcon added

---
+++ GroceryListApp/GroceryListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryListApp
+//
+//  Created by Dhruv Patel on 23/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryListApp/GroceryListApp/GroceryListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListAppApp.swift
+//  GroceryListApp
+//
+//  Created by Dhruv Patel on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
