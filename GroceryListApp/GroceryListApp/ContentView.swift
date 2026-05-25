55d153d93f066142c93f4360e632d6fe34c97763
Dhruv Patel <dhruvpatel913@gmail.com>
2026-05-24 19:20:56 -0700

    Environment variable added and the query variable added.

--- GroceryListApp/GroceryListApp/ContentView.swift
+++ GroceryListApp/GroceryListApp/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
     var body: some View {
         VStack {
             Image(systemName: "globe")
