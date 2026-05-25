cd276d7a549d641f3f98fda5f44c75843224cb4b
Dhruv Patel <dhruvpatel913@gmail.com>
2026-05-24 20:00:20 -0700

    Task completion when swipe from left to right feature added.

--- GroceryListApp/GroceryListApp/ContentView.swift
+++ GroceryListApp/GroceryListApp/ContentView.swift
@@ -40,6 +40,11 @@ struct ContentView: View {
                                 Label("Delete", systemImage: "trash")
                             }
                         }
+                        .swipeActions(edge: .leading)  {
+                            Button("Done", systemImage: "checkmark.circle") {
+                                item.isCompleted.toggle()
+                            }
+                        }
                 }
             }
             .navigationTitle("Grocery List")
